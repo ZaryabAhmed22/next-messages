308c1488691574969d4a644840739a223abca32b
Zaryab Ahmed Khan <zaryabzak22@gmail.com>
2025-03-24 03:02:10 +0500

    revalidatePath and revalidateTag

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,14 +1,22 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-import { addMessage } from '@/lib/messages';
+import { addMessage } from "@/lib/messages";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
-    'use server';
+    "use server";
 
-    const message = formData.get('message');
-    addMessage(message);
-    redirect('/messages');
+    const message = formData.get("message");
+    // addMessage(message);
+
+    // managing the full route chache. This is the best way when you want to refetch the data based on some action
+    revalidatePath("/messages", "layout");
+
+    // we can configure thr api requests and provide them tags. On calling this function, the next js throws away any cache related to this tag
+    revalidateTag("msg");
+
+    redirect("/messages");
   }
 
   return (
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,10 +1,17 @@
 import Messages from "@/components/messages";
+import { unstable_noStore } from "next/cache";
+
+// Next JS will automatically pick this setting for all the components in this file. With this settings you don't need to set the config on the requests
+// export const revalidate = 5;
+// export const dynamic = "force-dynamic"
 
 export default async function MessagesPage() {
+  // unstable_noStore()
   const response = await fetch("http://localhost:8080/messages", {
     // cache: 'no-store',
     next: {
-      revalidate: 5,
+      // revalidate: 5,
+      tags: ["msg"],
     },
   });
   const messages = await response.json();
